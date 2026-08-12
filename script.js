535c1e72db7ba8dc84422e7360f8081bd9a1a862
Guilherme Xavier <xavierguilherme.ti@gmail.com>
2026-08-12 16:13:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const menuButton=document.querySelector(".menu-btn");const menu=document.querySelector("#menu");
+menuButton.addEventListener("click",()=>menu.classList.toggle("open"));
+document.querySelectorAll("#menu a").forEach(link=>link.addEventListener("click",()=>menu.classList.remove("open")));
